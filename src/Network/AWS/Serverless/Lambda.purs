e2611671abbd83adc9c56f581721b7bd070e9e45
rinse <rinse@neko2.net>
2020-02-16 17:21:31 +0900

    add LambdaHandler1 and LambdaHandler2
      * remove LambdaHandler
      * change a type of lambdaMain1 and lambdaMain2

--- src/Network/AWS/Serverless/Lambda.purs
+++ src/Network/AWS/Serverless/Lambda.purs
@@ -1,29 +1,31 @@
 module Network.AWS.Serverless.Lambda
-    ( LambdaHandler
+    ( LambdaHandler1
+    , LambdaHandler2
     , lambdaMain1
     , lambdaMain2
     ) where
 
 import Control.Promise       (fromAff, Promise)
 import Effect.Aff            (Aff)
-import Effect.Aff.Compat     (mkEffectFn2)
-import Effect.Uncurried      (EffectFn2)
+import Effect.Aff.Compat     (mkEffectFn1, mkEffectFn2)
+import Effect.Uncurried      (EffectFn1, EffectFn2)
 import Foreign.Generic.Class (class Decode)
 import Prelude               (Void, const, flip, map, (<<<))
 
 
 -- |A two-argumented function type for handlers of AWS Lambda.
-type LambdaHandler a b c = EffectFn2 a b (Promise c)
+type LambdaHandler1 a b = EffectFn1 a (Promise b)
+type LambdaHandler2 a b c = EffectFn2 a b (Promise c)
 
 -- |Processes incoming events using the given function.
 lambdaMain1 :: forall a b. Decode a => Decode b
             => (a -> Aff b)
-            -> LambdaHandler a Void b
-lambdaMain1 = lambdaMain2 <<< const
+            -> LambdaHandler1 a b
+lambdaMain1 = mkEffectFn1 <<< (fromAff <<< _)
 
 -- |Processes incoming events using the given function.
 -- |This variety passes context to the function as well.
 lambdaMain2 :: forall a b c. Decode a => Decode b => Decode c
             => (b -> a -> Aff c)
-            -> LambdaHandler a b c
+            -> LambdaHandler2 a b c
 lambdaMain2 = mkEffectFn2 <<< flip <<< (map fromAff <<< _)
