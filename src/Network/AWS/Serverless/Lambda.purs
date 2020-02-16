a5e7696c11283c9799e32faa0bf511d6aa2e6e37
rinse <rinse@neko2.net>
2020-02-16 15:51:39 +0900

    add a restriction for lambda handler

--- src/Network/AWS/Serverless/Lambda.purs
+++ src/Network/AWS/Serverless/Lambda.purs
@@ -4,21 +4,26 @@ module Network.AWS.Serverless.Lambda
     , lambdaMain2
     ) where
 
-import Control.Promise   (fromAff, Promise)
-import Effect.Aff        (Aff)
-import Effect.Aff.Compat (mkEffectFn2)
-import Effect.Uncurried  (EffectFn2)
-import Prelude           ((<<<), const, flip, map)
+import Control.Promise       (fromAff, Promise)
+import Effect.Aff            (Aff)
+import Effect.Aff.Compat     (mkEffectFn2)
+import Effect.Uncurried      (EffectFn2)
+import Foreign.Generic.Class (class Decode)
+import Prelude               (Void, const, flip, map, (<<<))
 
 
 -- |A two-argumented function type for handlers of AWS Lambda.
 type LambdaHandler a b c = EffectFn2 a b (Promise c)
 
 -- |Processes incoming events using the given function.
-lambdaMain1 :: forall a b c. (a -> Aff c) -> LambdaHandler a b c
+lambdaMain1 :: forall a b. Decode a => Decode b
+            => (a -> Aff b)
+            -> LambdaHandler a Void b
 lambdaMain1 = lambdaMain2 <<< const
 
 -- |Processes incoming events using the given function.
 -- |This variety passes context to the function as well.
-lambdaMain2 :: forall a b c. (b -> a -> Aff c) -> LambdaHandler a b c
+lambdaMain2 :: forall a b c. Decode a => Decode b => Decode c
+            => (b -> a -> Aff c)
+            -> LambdaHandler a b c
 lambdaMain2 = mkEffectFn2 <<< flip <<< (map fromAff <<< _)
